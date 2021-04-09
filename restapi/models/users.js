c1c8914416ac9d383cd034ca6e3e765c8ef56842
UO270571 <uo270571@uniovi.es>
2021-04-09 18:28:23 +0200

    Cambio en esquema de usuarios con localizaciones

--- restapi/models/users.js
+++ restapi/models/users.js
@@ -1,25 +1,29 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
+// Esquema para las localizaciones de los usuarios
+const pointSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    enum: ['Point'],
+    required: true
+  },
+  coordinates: {
+    type: [Number],
+    required: true
+  }
+});
+
+// Esquema para los usuarios con sus localizaciones
 const schema = mongoose.Schema({
     URL: String,
     location: {
-        type: {
-          type: String,
-          enum: ['Point'],
-          required: true
-        },
-        coordinates: {
-          type: [Number],
-          required: true
-        }
+      type: pointSchema,
+      index: '2dsphere'
     },
     altitud: Number,
     longitud: Number,
     latitud: Number,
     fecha: Date
-})
+});
 
-// Crea un índice para las consultas por cercanía
-schema.index({ location: '2dsphere' });
-
-module.exports = mongoose.model("User", schema)
+module.exports = mongoose.model("User", schema);
