3d977b61ed7980f26a962c638b7b6d88c0322ee9
UO265181 <uo265181@uniovi.es>
2021-04-11 19:35:34 +0200

    Error import no usado

--- webapp/src/components/AddFriend/AddFriend.js
+++ webapp/src/components/AddFriend/AddFriend.js
@@ -2,7 +2,6 @@ import Button from "react-bootstrap/Button";
 import React from 'react';
 import Form from "react-bootstrap/Form";
 import Alert from "react-bootstrap/Alert";
-import ldflex from "@solid/query-ldflex";
 import { addFriend } from "./addFriendFunction";
 
 
