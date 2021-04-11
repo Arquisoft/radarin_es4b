0b2d84e058a4b06d71fe27522e6f1171b7e28659
UO266640 <jony23699@gmail.com>
2021-04-11 18:44:36 +0200

    Test mobile app

--- mobileapp/__tests__/App-test.js
+++ mobileapp/__tests__/App-test.js
@@ -5,10 +5,27 @@
 import 'react-native';
 import React from 'react';
 import App from '../App';
+import getText from '../src/i18n.js';
+import { getLanguages } from 'react-native-i18n';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   renderer.create(<App />);
+});
+
+
+it('applies the expected language', () => {
+
+  getLanguages().then(languages => {
+    if (languages == 'en')
+      expect(getText('location')
+      ).toBe('Send my location');
+
+    else if (languages == 'es')
+      expect(getText('location')
+      ).toBe('Enviar mi ubicación');
+
+  });
 });
