af82a6bc85a8632e048d2cc3c70658461ae23da4
uo270824 <uo270824@uniovi.es>
2021-04-24 02:03:33 +0200

    Cambio estructura internacionalización app web

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -3,12 +3,36 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './i18n'; 
+import { I18nextProvider } from "react-i18next";
+import i18next from "i18next";
 
+import enTranslation from './translations/en.json'; 
+import esTranslation from './translations/es.json'; 
+
+const resources = {
+    en: {
+        translation: enTranslation, 
+    },
+    es: {
+        translation: esTranslation,
+    }
+}; 
+
+
+i18next.init({
+  resources,
+  lng: 'es',
+  keySeparator: false, 
+  interpolation: {
+      escapeValue: false,
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
