d7c36d542121a32249016d9ead4ea6f414feac08
uo270824 <uo270824@uniovi.es>
2021-04-24 02:47:32 +0200

    Vuelta a test inicial

--- webapp/src/App.test.js
+++ webapp/src/App.test.js
@@ -1,11 +1,8 @@
-import 'react-native';
-import React from 'react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
- // Note: test renderer must be required after react-native.
- import renderer from 'react-test-renderer';
-
-test('app renders correctly', () => {
-  const app = renderer.create(<App />).toJSON();
-  expect(app).toMatchSnapshot();
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Bienvenido/i);
+  expect(linkElement).toBeInTheDocument();
 });
