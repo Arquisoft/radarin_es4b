b0ee0e5b51cfc4d71e687bf1a7973a14aac97af3
uo270824 <uo270824@uniovi.es>
2021-04-24 02:22:09 +0200

    Prueba cambio tests app web

--- webapp/src/App.test.js
+++ webapp/src/App.test.js
@@ -1,9 +1,6 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
-import i18n from 'i18next'; 
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(i18n.t('welcome.bienvenida'));
-  expect(linkElement).toBeInTheDocument();
+test('app renders correctly', () => {
+  const app = renderer.create(<App />).toJSON();
+  expect(app).toMatchSnapshot();
 });
