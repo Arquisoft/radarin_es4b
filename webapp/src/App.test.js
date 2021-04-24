d91cc07306e0d6113ed1ee1be20201b6d34ca072
uo270824 <uo270824@uniovi.es>
2021-04-24 02:27:21 +0200

    Prueba 2 cambio tests app web

--- webapp/src/App.test.js
+++ webapp/src/App.test.js
@@ -1,4 +1,9 @@
+import 'react-native';
+import React from 'react';
 import App from './App';
+
+ // Note: test renderer must be required after react-native.
+ import renderer from 'react-test-renderer';
 
 test('app renders correctly', () => {
   const app = renderer.create(<App />).toJSON();
