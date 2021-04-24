d6c4ccc2a323fb3507d2b35be634734884655de6
uo270824 <uo270824@uniovi.es>
2021-04-24 03:24:14 +0200

    Internacionalizacón de la página de bienvenida en la app web

--- webapp/src/components/Welcome.js
+++ webapp/src/components/Welcome.js
@@ -7,8 +7,8 @@ class Welcome extends React.Component {
       <div>
         <img src="/img/radarinLogo.png" alt="inrupt" />
         <h1 className="h1Card">{ getText("welcome.bienvenida") }</h1>
-        <h5 className="h5Card">Radarin es un sistema para facilitar encuentros entre amigos utilizando nuevas tecnologías.
-        <h6><br></br>El sistema es compatible con el proyecto <a href="https://en.wikipedia.org/wiki/Solid_(web_decentralization_project">SOLID </a>y no almacena información personal sobre los usuarios de forma centralizada. </h6> </h5>
+        <h5 className="h5Card">{ getText("welcome.descripcion") }
+        <h6><br></br>{ getText("welcome.info1") }<a href="https://en.wikipedia.org/wiki/Solid_(web_decentralization_project">SOLID </a>{ getText("welcome.info2") }</h6> </h5>
       </div>
     );
   }
