0501d10e7d5aaf2d77ab90558f11cc70c97b486b
uo270824 <uo270824@uniovi.es>
2021-04-28 01:57:58 +0200

    Vuelta a estado inicial

--- webapp/e2e/steps/register-form.steps.js
+++ webapp/e2e/steps/register-form.steps.js
@@ -1,17 +1,10 @@
 const {defineFeature, loadFeature}=require('jest-cucumber');
 const feature = loadFeature('./features/register-form.feature');
-const puppeteer = require('puppeteer');
 
 defineFeature(feature, test => {
   
   beforeEach(async () => {
-        //await global.page.goto('http://localhost:3000'); 
-        const browser = await puppeteer.launch({
-          headless: false,
-          defaultViewport: null
-        });
-        page = await browser.newPage();
-        await page.goto('http://localhost:3000', { waitUntil: 'load', timeout: 0 });
+    await global.page.goto('http://localhost:3000')
   })
 
   test('The user is not registered in the site and wants to see his friends list', ({given,when,then}) => {
