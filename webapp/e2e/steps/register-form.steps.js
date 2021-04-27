5ecf518e4b0301cba6ce4c7119cd3dc71060d278
uo270824 <uo270824@uniovi.es>
2021-04-28 01:36:42 +0200

    Prueba de aceptacion arreglo

--- webapp/e2e/steps/register-form.steps.js
+++ webapp/e2e/steps/register-form.steps.js
@@ -1,16 +1,21 @@
-const { defineFeature, loadFeature } = require('jest-cucumber');
+const {defineFeature, loadFeature}=require('jest-cucumber');
 const feature = loadFeature('./features/register-form.feature');
 const puppeteer = require('puppeteer');
 
-
 defineFeature(feature, test => {
-
+  
   beforeEach(async () => {
-    await global.page.goto('http://localhost:3000');
+        //await global.page.goto('http://localhost:3000'); 
+        const browser = await puppeteer.launch({
+          headless: false,
+          defaultViewport: null
+        });
+        page = await browser.newPage();
+        await page.goto('http://localhost:3000', { waitUntil: 'load', timeout: 0 });
   })
 
-  test('The user is not registered in the site and wants to see his friends list', ({ given, when, then }) => {
-
+  test('The user is not registered in the site and wants to see his friends list', ({given,when,then}) => {
+    
     let webID;
     let username;
     let password;
@@ -26,8 +31,6 @@ defineFeature(feature, test => {
     });
 
     then('The user will access to his friends list', async () => {
-
-
     });
   });
 
