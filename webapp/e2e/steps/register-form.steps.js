0c11f211afd8eb4e8684d1333ecf662291f8142e
uo270824 <uo270824@uniovi.es>
2021-04-28 10:14:14 +0200

    Prueba ejecucion prueba de aceptacion

--- webapp/e2e/steps/register-form.steps.js
+++ webapp/e2e/steps/register-form.steps.js
@@ -1,14 +1,14 @@
-const {defineFeature, loadFeature}=require('jest-cucumber');
+const { defineFeature, loadFeature } = require('jest-cucumber');
 const feature = loadFeature('./features/register-form.feature');
 
 defineFeature(feature, test => {
-  
+
   beforeEach(async () => {
     await global.page.goto('http://localhost:3000')
   })
 
-  test('The user is not registered in the site', ({given,when,then}) => {
-    
+  test('The user is not registered in the site', ({ given, when, then }) => {
+
     let webID;
     let username;
     let password;
@@ -20,13 +20,9 @@ defineFeature(feature, test => {
     });
 
     when('I fill the data in the form and press submit', async () => {
-      //await expect(page).toMatch('Hi, ASW students')
-      //await expect(page).toFillForm('form[name="register"]', {
-      //  username: username,
-      //  email: email,
-      //})
-      //await expect(page).toClick('button', { text: 'Submit' })
-      //await expect(page).toMatch('Welcome to ASW')
+
+      // Cambiamos el idioma a español 
+      await expect(page).toClick('button', { text: 'Idioma' || 'Language' });
     });
 
     then('A welcome message should be shown in the screen', async () => {
@@ -34,16 +30,16 @@ defineFeature(feature, test => {
   });
 
   test('The user is already registered in the site', ({ given, when, then }) => {
-    
+
     given('An already registered user', () => {
     });
 
     when('I fill the data in the form and press submit', async () => {
-      
+
     });
 
     then('An error message should be shown in the screen', async () => {
     });
-    
+
   });
 });
