e1ddbad618850412c4b60967854cd9fede85e7af
uo270824 <uo270824@uniovi.es>
2021-04-30 20:51:53 +0200

    Correccion anterior commit

--- webapp/src/__tests__/App.test.js
+++ webapp/src/__tests__/App.test.js
@@ -1,9 +1,8 @@
-import { render, cleanup } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from '../App';
 // import { getText } from '../translations/i18n'; 
 
 test('app renders correctly', () => {
-  afterAll(cleanup);
   const { container } = render(<App />);
-  expect(container).toBeTruthy();
+  expect(container).toMatchSnapshot();
 });
