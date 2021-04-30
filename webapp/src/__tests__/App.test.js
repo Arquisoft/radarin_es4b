a2af40ee650e44f307f349c4fbe799d6c92d2b4f
uo270824 <uo270824@uniovi.es>
2021-04-30 21:07:28 +0200

    Tests APP app web

--- webapp/src/__tests__/App.test.js
+++ webapp/src/__tests__/App.test.js
@@ -1,8 +1,21 @@
-import { render } from '@testing-library/react';
+import { render, cleanup, screen  } from '@testing-library/react';
 import App from '../App';
-// import { getText } from '../translations/i18n'; 
+
+test.afterEach(cleanup)
 
 test('app renders without crashing', () => {
   const { container } = render(<App />);
   expect(container).toBeTruthy();
+});
+
+test('applies the expected language', () => {
+  var lng = navigator.language || navigator.userLanguage;
+  if(lng.startsWith("es")) {
+    const linkElement = screen.getByText("¡ Bienvenido a Radarin !");
+    expect(linkElement).toBeInTheDocument();
+  }
+  else {
+    const linkElement = screen.getByText("Welcome to Radarin !");
+    expect(linkElement).toBeInTheDocument();
+  }
 });
